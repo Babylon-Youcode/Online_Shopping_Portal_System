fba4325f96c48be17e552211a9fff336306e0c6e
khalid raqi <khaaliid.raaqii@gmail.com>
2021-08-10 22:36:05 +0100

    Code App Nouveau Table

--- App/app/Http/Controllers/Admin/CategoryController.php
+++ App/app/Http/Controllers/Admin/CategoryController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
-use App\Models\Category;
+use App\Http\Controllers\Controller;
+use App\Models\Admin\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Storage;
--- App/resources/views/admin/manage_brand.blade.php
+++ App/resources/views/admin/manage_brand.blade.php
@@ -39,25 +39,33 @@
                                         <form action="{{route('brand.manage_brand_process')}}" method="post" enctype="multipart/form-data">
                                             @csrf
                                             <div class="form-group">
-                                                <label for="color" class="control-label mb-1">Brand </label>
-                                                <input id="brand" value="{{$name}}" name="name" type="text" class="form-control" aria-required="true" aria-invalid="false" required>
-                                                @error('name')
-                                                <div class="alert alert-danger" role="alert">
-                                                    {{$message}}		
+                                                <div class="row">
+                                                    <div class="col-lg-8">
+                                                        <label for="color" class="control-label mb-1">Brand </label>
+                                                        <input id="brand" value="{{$name}}" name="name" type="text" class="form-control" aria-required="true" aria-invalid="false" required>
+                                                        @error('name')
+                                                        <div class="alert alert-danger" role="alert">
+                                                            {{$message}}		
+                                                        </div>
+                                                        @enderror 
+                                                    </div>
+                                                    <div class="col-lg-142 my-4 mx-4">
+                                                        <label for="image" class="control-label mb-1"> Show in Home Page</label>
+                                                        <input id="is_home" name="is_home" type="checkbox" class="form-control" {{$is_home_selected}}>
+                                                    </div>
                                                 </div>
-                                                @enderror
                                             </div>
                                             <div class="form-group">
                                                 <label for="image" class="control-label mb-1"> Image</label>
-                                                <input id="image" name="image" type="file" class="form-control" aria-required="true" aria-invalid="false" {{$image_required}}>
+                                                <input id="image" name="image" type="file" class="form-control" aria-required="true" aria-invalid="false">
                                                 @error('image')
                                                 <div class="alert alert-danger" role="alert">
                                                 {{$message}}		
                                                 </div>
                                                 @enderror
-
+            
                                                 @if($image!='')
-                                                    <a href="{{asset('storage/media/brand/'.$image)}}"><img width="100px" src="{{asset('storage/media/brand/'.$image)}}" class="my-4"/></a>
+                                                    <img width="100px" src="{{asset('storage/media/brand/'.$image)}}"/>
                                                 @endif
                                             </div>
                                             <div>
